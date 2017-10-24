050f02da1a845cb855dc41ad83d9e5910b5f710f
Shen9ke <2592528345@qq.com>
2017-10-24 14:25:06 +0800

    commit

---
+++ src/settings.js
@@ -0,0 +1,93 @@
+_CCSettings = {
+    platform: "web-desktop",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    rawAssets: {
+        assets: {
+            "a9wSI/v9xJO4HgALd/QEqa": [
+                "picture/background.png",
+                0
+            ],
+            "beTgBhYlBAIq1aE50oOWtn": [
+                "picture/button.png",
+                0
+            ],
+            "c4+XQudTtGoolWkdKaWHnz": [
+                "picture/setting_bg.png",
+                0
+            ]
+        },
+        internal: {
+            "71VhFCTINJM6/Ky3oX9nBT": [
+                "image/default_btn_disabled.png",
+                0
+            ],
+            "e8Ueib+qJEhL6mXAHdnwbi": [
+                "image/default_btn_normal.png",
+                0
+            ],
+            "b4P/PCArtIdIH38t6mlw8Y": [
+                "image/default_btn_pressed.png",
+                0
+            ]
+        }
+    },
+    assetTypes: [
+        "cc.Texture2D"
+    ],
+    launchScene: "db://assets/scence/menu.fire",
+    scenes: [
+        {
+            url: "db://assets/scence/menu.fire",
+            uuid: 1
+        },
+        {
+            url: "db://assets/scence/main.fire",
+            uuid: 0
+        },
+        {
+            url: "db://assets/scence/load.fire",
+            uuid: "adi9Bk1MtGwpYOjOiFw8DX"
+        }
+    ],
+    packedAssets: {
+        "03e05d51a": [
+            "83r/svudJGkIzP6SNPrz56",
+            "bc8J9+rRxNYZ9W7TObUbo1",
+            0,
+            2
+        ],
+        "0c32cbba4": [
+            "29FYIk+N1GYaeWH/q1NxQO",
+            "80pP8jCR9LWLbtY4m7nTF4",
+            1,
+            2,
+            "e97GVMl6JHh5Ml5qEDdSGa",
+            "f0BIwQ8D5Ml7nTNQbh1YlS",
+            "f2G4IlJKhAjp32+oQ4aPXv"
+        ]
+    },
+    orientation: "",
+    uuids: [
+        "cajYTzSzpDPqD2chfHR2h2",
+        "b35Ew/mRRAk43UTc/lJTWC",
+        "d54I2ImHZFQYgNRQmP4UXL"
+    ],
+    "md5AssetsMap": {
+        "03/03e05d51a.json": "80989",
+        "0c/0c32cbba4.json": "c30ca",
+        "ad/ad8bd064-d4cb-46c2-960e-8ce885c3c0d7.json": "00264",
+        "assets/picture/background.png": "0ee67",
+        "assets/picture/button.png": "4cc5c",
+        "assets/picture/setting_bg.png": "62039",
+        "internal/image/default_btn_disabled.png": "286c6",
+        "internal/image/default_btn_normal.png": "1ecb7",
+        "internal/image/default_btn_pressed.png": "bedf4"
+    }
+}
